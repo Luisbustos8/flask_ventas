3559fabe61124e6065228cff55c108c1f4e88b8d
Luis Alejandro Bustos Lucas <Luis@MacBook-Pro-de-Luis-5.local>
2020-05-26 18:57:01 +0200

    Json

--- run.py
+++ run.py
@@ -1,8 +1,24 @@
 from flask import Flask
-
+import csv
 
 app = Flask(__name__) #Creamos nuestra aplicación
 
 @app.route("/")
 def index():
-    return "Hola, mundo"
+    fventas = open ("./sales10.csv", "r")
+    csvreader = csv.reader(fventas, delimiter=",")
+
+    registros = []
+    d = {}
+    for linea in csvreader:
+        registros.append(linea)
+        if linea[0] in d:
+            d[linea[0]]["ingresos"] += float(linea[11])
+            d[linea[0]]["beneficios"] += float(linea[13])
+
+        else:
+            if linea[0] != "region":
+                d[linea[0]] = {"ingresos": float(linea[11]), "beneficios": float(linea[13])}
+   
+
+    return d
--- test_flask_app.py
+++ test_flask_app.py
@@ -6,7 +6,7 @@ class TestWeb(TestFlaskBase): #Hereda de testFlaskBase
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
-    def test_route_index_is_hola_mundo(self):
+    def test_route_index_is_lista_regiones(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, b"Hola, mundo")
