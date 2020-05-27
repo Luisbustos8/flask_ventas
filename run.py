ddc3ab870fca0271cb68c1c4cd6af9d780c5d61d
Luis Alejandro Bustos Lucas <Luis@MacBook-Pro-de-Luis-5.local>
2020-05-27 13:59:53 +0200

    Ruta paises, plantilla paises

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ def index():
     fventas = open ("./sales.csv", "r") #enrutamos la librería.
     csvreader = csv.reader(fventas, delimiter=",") #leemos y delimitamos hasta la coma
 
-    registros = []
+   
     d = {}
     for linea in csvreader:
         registros.append(linea)
@@ -22,6 +22,26 @@ def index():
    
 
     return render_template("region.html", ventas=d)
+
+@app.route("/paises")
+def paises():
+    region_name = request.values["region"]
+
+    fventas = open("./sales.csv", "r")
+    csvreader= csv.reader(fventas, delimiter=",")
+
+    d = {}
+    for linea in csvreader:
+        if linea[0] == region_name: #Es pais igual al pais que deseamos
+            if linea [1] in d: #si está region en diccionario
+                d[linea[1]]["ingresos"]+= round(float(linea[11]),2) #obtener y añadir valor ingresos
+                d[linea[1]]["beneficios"] += round(float(linea[13]),2) #obtener y añadir valor beneficios
+            else: #si no está en el diccionario me lo creas y guardas región, ingresos y beneficios
+                d[linea[1]] = {"ingresos": round(float(linea[11]),2), "beneficios": round(float(linea[13]),2)}
+   
+
+
+    return render_template("paises.html", ventas=d)
 
 
 
