16e9cebf86f1641147d1ef7053401273ac0f1b31
Luis Alejandro Bustos Lucas <Luis@MacBook-Pro-de-Luis-5.local>
2020-05-28 22:07:04 +0200

    enlaces entre paginas

--- run.py
+++ run.py
@@ -11,14 +11,13 @@ def index():
    
     d = {}
     for linea in csvreader:
-        registros.append(linea)
         if linea[0] in d:
-            d[linea[0]]["ingresos"]+= round(float(linea[11]),2)
-            d[linea[0]]["beneficios"] += round(float(linea[13]),2)
+            d[linea[0]]["ingresos"]+= float(linea[11])
+            d[linea[0]]["beneficios"] += float(linea[13])
 
         else:
             if linea[0] != "region":
-                d[linea[0]] = {"ingresos": round(float(linea[11]),2), "beneficios": round(float(linea[13]),2)}
+                d[linea[0]] = {"ingresos": float(linea[11]), "beneficios": float(linea[13])}
    
 
     return render_template("region.html", ventas=d)
@@ -28,20 +27,20 @@ def paises():
     region_name = request.values["region"]
 
     fventas = open("./sales.csv", "r")
-    csvreader= csv.reader(fventas, delimiter=",")
+    csvreader= csv.reader(fventas, delimiter=",")#Creas variable de fventas y lo delimitas por una coma.
 
     d = {}
     for linea in csvreader:
         if linea[0] == region_name: #Es pais igual al pais que deseamos
             if linea [1] in d: #si está region en diccionario
-                d[linea[1]]["ingresos"]+= round(float(linea[11]),2) #obtener y añadir valor ingresos
-                d[linea[1]]["beneficios"] += round(float(linea[13]),2) #obtener y añadir valor beneficios
+                d[linea[1]]["ingresos"]+= float(linea[11]) #obtener y añadir valor ingresos
+                d[linea[1]]["beneficios"] += float(linea[13]) #obtener y añadir valor beneficios
             else: #si no está en el diccionario me lo creas y guardas región, ingresos y beneficios
-                d[linea[1]] = {"ingresos": round(float(linea[11]),2), "beneficios": round(float(linea[13]),2)}
+                d[linea[1]] = {"ingresos": float(linea[11]), "beneficios": float(linea[13])}
    
 
 
-    return render_template("paises.html", ventas=d)
+    return render_template('pais.html', ventas_pais=d, region_nm=request.values['region'])
 
 
 
